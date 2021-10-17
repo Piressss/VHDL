47c5000208d774cbb00a53b67f5a22fe6c974b04
Felipe Martins Pires <felipe.mp87@gmail.com>
2021-10-17 19:25:21 -0300

    Update fifo_single.vhd
    Finished the implementation.

--- libs/fifo_lib/fifo_single.vhd
+++ libs/fifo_lib/fifo_single.vhd
@@ -28,13 +28,17 @@ entity fifo_single is
         data_rd_o           : out std_logic_vector(data_width_g-1 downto 0);
         --
         overflow_o          : out std_logic;
-        underflow_o         : out std_logic
+        underflow_o         : out std_logic;
+        empty_o             : out std_logic;
+        full_o              : out std_logic
     );
 end fifo_single;
 
 architecture rtl of fifo_single is
     
     signal data_cnt         : unsigned(vec_fit(FIFO_DEPTH_G)-1 downto 0) := (others => '0');
+    signal wr_addr_s        : unsigned(vec_fit(FIFO_DEPTH_G)-1 downto 0) := (others => '0');
+    signal rd_addr_s        : unsigned(vec_fit(FIFO_DEPTH_G)-1 downto 0) := (others => '0');
     
 begin
     
@@ -80,6 +84,58 @@ begin
         end if;
     end process;
             
+    -- Empty Signal
+    empty_p: process(clk_i)
+    begin
+        if rising_edge(clk_i) then
+            if rst_i = '1' then
+                empty_o <= '1';
+            elsif data_cnt = 0 then
+                empty_o <= '1';
+            else
+                empty_o <= '0';
+            end if;
+        end if;
+    end process;
+            
+    -- Full Signal
+    full_p: process(clk_i)
+    begin
+        if rising_edge(clk_i) then
+            if rst_i = '1' then
+                full_o <= '0';
+            elsif data_cnt = FIFO_DEPTH_G-2 then
+                full_o <= '1';
+            elsif data_cnt = FIFO_DEPTH_G-3 then
+                full_o <= '0';
+            end if;
+        end if;
+    end process;
+            
+    -- Write Address
+    wr_addr_p: process(clk_i)
+    begin
+        if rising_edge(clk_i) then
+            if rst_i = '1' then
+                wr_addr_s <= (others => '0');
+            elsif we_i = '1' then
+                wr_addr_s <= wr_addr_s + 1;
+            end if;
+        end if;
+    end process;
+            
+    -- Read Address
+    rd_addr_p: process(clk_i)
+    begin
+        if rising_edge(clk_i) then
+            if rst_i = '1' then
+                rd_addr_s <= (others => '0');
+            elsif re_i = '1' then
+                rd_addr_s <= rd_addr_s + 1;
+            end if;
+        end if;
+    end process;
+            
     -- Memory
     mem_u: entity mem_lib.ram
         generic map(
@@ -94,7 +150,12 @@ begin
             --
             we_i                => we_i,
             data_wr_i           => data_wr_i,
-            addr_wr_i           => ,
+            addr_wr_i           => std_logic_vector(wr_addr_s),
             --
+            re_i                => re_i,
+            addr_rd_i           => std_logic_vector(rd_addr_s),
+            data_valid_o        => data_valid_o,
+            data_rd_o           => data_rd_o
+        );
     
 end rtl;
